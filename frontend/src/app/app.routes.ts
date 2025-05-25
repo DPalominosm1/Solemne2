7bb5e6f1f85c0fa5d6a623854d6da11a91bc23fa
DPalominosm1 <dpalominosm1@correo.uss.cl>
2025-05-25 00:31:38 -0400

    tercer commit, ahora deberia funcionar el boton

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AppComponent } from './app.component';
+import { TaskManagerComponent } from './task-manager/task-manager.component';
 
 
 export const routes: Routes = [
@@ -10,8 +10,9 @@ export const routes: Routes = [
         title: 'Login Page',
     },
     {
-        path: 'main/:id',
-        component: AppComponent,
-        title: 'Main Page',
+        path: 'taskManager/:id',
+        component: TaskManagerComponent, 
+        title: 'Task Manager Pro',
+        //problema: es appcomponent, y appcomponent lo estoy usando para hacer display de todo, crear nuevo componente para pagina principal
     }
 ];
---
+++ frontend/src/app/task-manager/task-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-task-manager',
+  imports: [RouterModule],
+  templateUrl: './task-manager.component.html',
+  styleUrl: './task-manager.component.css'
+})
+export class TaskManagerComponent {
+
+}
